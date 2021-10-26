a669abc3672087fd7031c02f474449733c4adf8a
rlay0003 <92511615+rlay0003@users.noreply.github.com>
2021-10-26 23:40:30 +1100

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var spec = "js/price_bar_chart.json";
+var spec = "/js/price_bar_chart.json";
 vegaEmbed('#bar_chart', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
