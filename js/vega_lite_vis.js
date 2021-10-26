d2ba17bbc7e9cf3d6e13e6d3a780b3bda15d43a4
rlay0003 <92511615+rlay0003@users.noreply.github.com>
2021-10-26 23:15:11 +1100

    Add files via upload
    added initial update for assignment 2

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,30 @@
+var spec = "js/price_bar_chart.json";
+vegaEmbed('#bar_chart', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var spec2 = "/js/price_brushing.json";
+vegaEmbed('#brush_chart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec3 = "/js/factors_matrix.json";
+vegaEmbed('#matrix', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec4 = "js/donut.json";
+vegaEmbed('#donut', spec4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec5 = "js/relationship.json";
+vegaEmbed('#relationship', spec5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec6 = "js/heatmap.json";
+vegaEmbed('#heatmap', spec6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
